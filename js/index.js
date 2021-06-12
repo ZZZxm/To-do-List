2e8640811ce3def4a12faa69311a1de4c5b6b638
Brian Chiu <brianzxm1223@gmail.com>
2021-06-13 01:14:06 +0800

    [Updated] index界面优化

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+// 存储各个todo分组列表
+// localStorage中todoLists存储分组列表
+// @Param
+// 		name: 列表名称
+//		num: 列表所含待办事项数量
+const todoLists = JSON.parse(localStorage.getItem("todoLists")) || [];
+
 // 原生js实现document.ready
 (function () {
     var ie = !!(window.attachEvent && !window.opera);
@@ -22,48 +29,59 @@
     };
  })();
 
-// 存储各个todo分组列表
-const todoLists = JSON.parse(localStorage.getItem("todoLists")) || [];
-
 document.ready(function() {
-	
+	// 根据localStorage生成列表
 	renderLists();
-	
-	// 为每个list绑定跳转页面事件
-	var lists = document.getElementsByClassName("list");
-    for (i = 0; i < lists.length; i++)
-    {
-        let listName = lists[i].id;
-        console.log(listName);
-        lists[i].onclick = function() {
-            window.location.href = "list.html?listname="+listName;
-        }
-    }
 	
 	document.querySelector('#add-btn')
 		.addEventListener('click', addList);
 })
 
+// 每次列表有更新时，重新生成列表
+function renderLists() {
+	const listsView = document.querySelector('#self-list');
+	listsView.innerHTML = todoLists.map(obj => listGenerator(obj)).join('');
+	
+	var sizes = document.getElementsByClassName("list-size");
+	console.log(sizes);
+	for (i = 0; i < sizes.length; i++) {
+		if (sizes[i].innerText == "0") {
+			sizes[i].style.display = "none";
+		}
+	}
+	
+	addListenerToList();
+}
+
 function addListenerToList() {
+	// 每个list添加跳转页面事件
 	var lists = document.getElementsByClassName("list");
-	for (i = 0; i < lists.length; i++)
-	{
+	for (i = 0; i < lists.length; i++) {
 	    let listName = lists[i].id;
 	    console.log(listName);
 	    lists[i].onclick = function() {
 	        window.location.href = "list.html?listname="+listName;
 	    }
 	}
+	
+	// list中的删除按钮
+	var del = document.querySelectorAll(".del");
+	del.forEach(d => d.addEventListener('click', deleteList));
+}
+
+function getUniqueId() {
+	return (Date.now && Date.now()) || new Date().getTime();
 }
 
 // 添加分组列表
-function addList(e) {
+function addList() {
 	let inputList = document.getElementById("add-list").value;
 	if (inputList == "") {
 		return;
 	}
 	
 	let listObj = {
+		id: getUniqueId(),
 		name: inputList,
 		num: 0,
 	};
@@ -74,18 +92,31 @@ function addList(e) {
 	renderLists();
 }
 
-function renderLists() {
-	const listsView = document.querySelector('#self-list');
-	listsView.innerHTML = todoLists.map(obj => listGenerator(obj)).join('');
-	addListenerToList();
+// 删除分组列表
+function deleteList(e) {
+	e.stopPropagation();
+	let parNode = e.currentTarget.parentNode;
+	if (!confirm("Are you sure to delete \"" + parNode.getAttribute('id') + "\"?")) {
+		return;
+	}
+	
+	let listId = parNode.getAttribute("uni-id");
+	let targetList = todoLists.find(list => list.id == listId);
+	let index = todoLists.indexOf(targetList);
+	todoLists.splice(index, 1);
+	localStorage.setItem("todoLists", JSON.stringify(todoLists));
+	renderLists();
 }
 
 function listGenerator(obj) {
 	return `
-	<div id="${obj.name}" class="list">
+	<div id="${obj.name}" class="list" uni-id="${obj.id}">
 		<img src="img/list-icon.png" class="icon">
 		<div class="list-name">
 			${obj.name}
+		</div>
+		<div class="del">
+			<img src="img/delete.png" class="del-icon">
 		</div>
 		<div class="list-size">
 			${obj.num}
